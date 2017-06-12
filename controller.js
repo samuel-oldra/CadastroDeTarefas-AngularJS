3c093fde5b9588036a9c6831bcb7c7b10afd626d
Samuel B. Oldra <samuel.oldra@gmail.com>
2017-06-12 00:00:00 -0300

    Refactoring

--- controller.js
+++ controller.js
@@ -1,18 +1,18 @@
 angular.module("minhaApp")
 
-.controller("meuController", ['$scope', function($scope) {
+    .controller("meuController", ['$scope', function ($scope) {
 
-    $scope.nome = "";
-
-    $scope.lista = [];
-
-    $scope.adicionar = function(){
-        $scope.lista.push($scope.nome);
         $scope.nome = "";
-    }
 
-    $scope.remover = function(index){
-        $scope.lista.splice(index, 1);
-    }
+        $scope.lista = [];
 
-}])
+        $scope.adicionar = function () {
+            $scope.lista.push($scope.nome);
+            $scope.nome = "";
+        }
+
+        $scope.remover = function (index) {
+            $scope.lista.splice(index, 1);
+        }
+
+    }])
